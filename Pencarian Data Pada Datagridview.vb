5cf70982cc19ee85801ad537ea595d2e1421c4ff
Sutrisno0505 <harianbekerja@gmail.com>
2025-05-27 21:26:22 +0700

    Add files via upload

---
+++ Pencarian Data Pada Datagridview.vb
@@ -0,0 +1,12 @@
+        Call koneksi()
+        t1.Text = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        t4.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        t7.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        t8.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value
+        t9.Text = DataGridView1.Rows(e.RowIndex).Cells(4).Value
+        t10.Text = DataGridView1.Rows(e.RowIndex).Cells(5).Value
+        t11.Text = DataGridView1.Rows(e.RowIndex).Cells(6).Value
+        t12.Text = DataGridView1.Rows(e.RowIndex).Cells(7).Value
+        t13.Text = DataGridView1.Rows(e.RowIndex).Cells(8).Value
+        Call tampildatamapelx()
+        Call tampildatasiswax()
